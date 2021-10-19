5ba3b5a7d905f8ce4fd97afe291a7759070781be
EnzoFrznLaval <enzo.forzani.1@ulaval.ca>
2021-10-19 08:51:30 -0400

    Add comment and change invalid port message

--- TP3_Q1.py
+++ TP3_Q1.py
@@ -9,7 +9,14 @@ from glosocket import *
 
 def get_arguments() -> Tuple[bool, bool, int, Optional[str]]:
     """
-    TODO REECRIRE le commentaire
+    Cette fonction doit :
+    - ajouter les arguments attendus aux parser,
+    - récupérer les arguments passés,
+    - retourner un tuple contenant dans cet ordre : 
+        1. est-ce que le protocole est IPv6 ? (Booléen)
+        2. est-ce que le mode est « écoute » ? (Booléen)
+        3. le port choisi (entier)
+        4. l’adresse du serveur (string si client, None si serveur)
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--port", dest="port", type=int,
@@ -56,7 +63,7 @@ def make_server_socket(port: int, est_ipv6: bool) -> socket.socket:
     try:
         server_socket.bind(("127.0.0.1", port))
     except:
-        print("Error: Port is invalid")
+        print("Error: Unable to bind to the port " + str(port))
         server_socket.close()
         sys.exit()
 
